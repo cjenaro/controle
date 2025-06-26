2246d63f2c2f02342b584891e52d1265f7ec767d
cjenaro <jen.calvineo@gmail.com>
2025-06-26 16:06:49 -0300

    fix: controller setup

--- src/templates/controller/controller.lua
+++ src/templates/controller/controller.lua
@@ -1,15 +1,27 @@
 -- {{class_name}} controller
-local BaseController = require("comando.base_controller")
+local BaseController = require("comando")
+local orbita = require("orbita")
 local {{model_name}} = require("app.models.{{model_file}}")
 
 --- @class {{class_name}} : BaseController
-local {{class_name}} = BaseController:extend()
+local {{class_name}} = {}
+{{class_name}}.__index = {{class_name}}
+setmetatable({{class_name}}, BaseController)
+
+-- Extend BaseController with Orbita methods
+{{class_name}} = orbita.extend_controller({{class_name}})
+
+function {{class_name}}:new(request)
+    local controller = BaseController:new(request)
+    setmetatable(controller, self)
+    return controller
+end
 
 -- GET /{{route_path}}
 function {{class_name}}:index()
     local {{plural_name}} = {{model_name}}:all()
     
-    self:render("{{view_path}}/index", {
+    return self:render_orbita("{{view_path}}/index", {
         {{plural_name}} = {{plural_name}}
     })
 end
@@ -22,28 +34,29 @@ function {{class_name}}:show()
         return self:not_found()
     end
     
-    self:render("{{view_path}}/show", {
+    return self:render_orbita("{{view_path}}/show", {
         {{singular_name}} = {{singular_name}}
     })
 end
 
 -- GET /{{route_path}}/new
-function {{class_name}}:new()
+function {{class_name}}:new_action()
     local {{singular_name}} = {{model_name}}:new()
     
-    self:render("{{view_path}}/new", {
+    return self:render_orbita("{{view_path}}/new", {
         {{singular_name}} = {{singular_name}}
     })
 end
 
 -- POST /{{route_path}}
 function {{class_name}}:create()
-    local {{singular_name}} = {{model_name}}:new(self.params.{{singular_name}})
+    local data = self:request_data()
+    local {{singular_name}} = {{model_name}}:new(data.{{singular_name}})
     
     if {{singular_name}}:save() then
-        self:redirect("/{{route_path}}/" .. {{singular_name}}.id)
+        return self:redirect("/{{route_path}}/" .. {{singular_name}}.id)
     else
-        self:render("{{view_path}}/new", {
+        return self:render_orbita("{{view_path}}/new", {
             {{singular_name}} = {{singular_name}},
             errors = {{singular_name}}.errors
         })
@@ -58,7 +71,7 @@ function {{class_name}}:edit()
         return self:not_found()
     end
     
-    self:render("{{view_path}}/edit", {
+    return self:render_orbita("{{view_path}}/edit", {
         {{singular_name}} = {{singular_name}}
     })
 end
@@ -71,10 +84,11 @@ function {{class_name}}:update()
         return self:not_found()
     end
     
-    if {{singular_name}}:update(self.params.{{singular_name}}) then
-        self:redirect("/{{route_path}}/" .. {{singular_name}}.id)
+    local data = self:request_data()
+    if {{singular_name}}:update(data.{{singular_name}}) then
+        return self:redirect("/{{route_path}}/" .. {{singular_name}}.id)
     else
-        self:render("{{view_path}}/edit", {
+        return self:render_orbita("{{view_path}}/edit", {
             {{singular_name}} = {{singular_name}},
             errors = {{singular_name}}.errors
         })
@@ -90,7 +104,7 @@ function {{class_name}}:destroy()
     end
     
     {{singular_name}}:destroy()
-    self:redirect("/{{route_path}}")
+    return self:redirect("/{{route_path}}")
 end
 
 return {{class_name}}
